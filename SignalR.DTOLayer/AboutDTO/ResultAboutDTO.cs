87ecf1ec4934d9942192560f2c21714eb2d5e866
aliemre <aliemre@EMRE327F>
2024-04-09 01:07:23 +0300

    DTO eklendi

---
+++ SignalR.DTOLayer/AboutDTO/ResultAboutDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DTOLayer.AboutDTO
+{
+    public class ResultAboutDTO  // her işlem kendisi içinde yapılsın. 
+    {
+        public int AboutID { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ SignalR.DTOLayer/SocialMediaDTO/CreateSocialMediaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DTOLayer.SocialMediaDTO
+{
+    public class CreateSocialMediaDTO
+    {
+       // public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; }
+        public string Icon { get; set; }
+    }
+}

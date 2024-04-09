8c4a2f501c1a85d9c005f9c43448474fbcfa8c97
aliemre <aliemre@EMRE327F>
2024-04-09 23:09:50 +0300

    .

--- SignalR.Api/Program.cs
+++ SignalR.Api/Program.cs
@@ -43,6 +43,7 @@ builder.Services.AddScoped<ITestimonialDAL, EFTestimonialDAL>();
 
 
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
